42eb79952115f69d5fa3249558c44ff39e52041b
prith <prithvinpr1001@gmail.com>
2025-11-06 17:57:53 +0530

    New code is added in PrithvinLogin

--- PracticeProject_GIT/src/test/java/gitpractice/PrithvinLogin.java
+++ PracticeProject_GIT/src/test/java/gitpractice/PrithvinLogin.java
@@ -4,6 +4,8 @@ public class PrithvinLogin {
 
 	public static void main(String[] args) {
 		System.out.println("All mighty push");
+		System.out.println("Akatsuki wa tsudotta");
+		System.out.println("ware ware wa Pain");
 	}
 
 }
